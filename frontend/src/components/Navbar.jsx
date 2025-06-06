838880cb524da8781de8274097d7eebdb5e10ec8
BTRON32 <131682089+BTRON32@users.noreply.github.com>
2025-06-06 21:01:11 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React, { useContext, useState } from 'react'
 import {assets} from "../assets/assets"
 import {Link, NavLink} from "react-router-dom"
 import { ShopContext } from '../context/ShopContext'
-import toast from "react-toastify"
+// import toast from "react-toastify"
 
 const Navbar = () => {
 
@@ -11,13 +11,12 @@ const Navbar = () => {
 
   {/* now we will add logout functionality */}
   const logout=()=>{
-     toast.success("Logged out successfully")
     navigate("/login")//navi
     // gate user on login page
     localStorage.removeItem("token")//remove token from localstorage, key name="token"
     setToken('')//clear state variable
     setCartItems({})//when we are loggedout we will clear cart item
-   
+    // toast.success("Logged out successfully")
   }
 
   return (
